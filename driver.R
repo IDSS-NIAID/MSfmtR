09b46710068011dad902070dfd1510ee56100d8d
Randy Johnson <johnsonra@mail.nih.gov>
2024-03-04 09:00:50 -0500

    First efficient working version

--- driver.R
+++ driver.R
@@ -19,10 +19,10 @@ files <- map_chr(dirs, ~ file.path('data', .x) |>
 paths <- file.path('data', dirs, files)
 
 # format styles
-protein_header <- createStyle(fgFill = "#A7CDF0")
-protein_rows   <- createStyle(fgFill = "#DDEBF7")
-peptide_header <- createStyle(fgFill = "#F0CBA8")
-peptide_rows   <- createStyle(fgFill = "#FCE4D6")
+protein_header_style <- createStyle(fgFill = "#A7CDF0")
+protein_rows_style   <- createStyle(fgFill = "#DDEBF7")
+peptide_header_style <- createStyle(fgFill = "#F0CBA8")
+peptide_rows_style   <- createStyle(fgFill = "#FCE4D6")
 
 # register parallel backend
 cl <- makeCluster(length(dirs))
@@ -183,60 +183,45 @@ foreach(j=1:length(paths)) %dopar%
     rm(data, peptides)
   }
   
-  if(!file.exists(paste0('output/', dirs[j], '.xlsx')))
-  {
   
+  ####################
+  # Write Excel file #
+  ####################
+  
+  if(file.exists(paste0('output/', dirs[j], '_wb.RData')))
+  {
+    load(paste0('output/', dirs[j], '_wb.RData'))
+  }else{
     wb <- createWorkbook()
   
     addWorksheet(wb, dirs[j])
-    # don't do this - allocating the entire object at once seems to slow things down rather than speed them up.
-    # not to mention the fact that the format of the written data is an issue - you can't use this package
-    # to format columns of data that are mixed numeric and text unless you write them all one block at a time.
-    # This is a problem because the data takes forreevvveerrr to write one block at a time. :P
-    # Either way I think this ends up rewriting `wb` on each call to `writeData`, which is super inefficient.
-    # writeData(wb, sheet = dirs[j], startRow = 1, startCol = 1,
-    #           x = matrix('', 
-    #                      nrow = dim(proteins)[1] +
-    #                        map_int(proteins$peptides, ~ dim(.x)[1]) %>% sum(),
-    #                      ncol = dim(proteins)[2] - 1))
-  
-    nextRow <- 1 # keep track of what row we're on
-  
+
+    # keep track of what row we're on
+    nextRow <- 1 
+    
+    # keep track of where we put things
+    indices <- list(protein_rows    = integer(), 
+                    peptide_headers = integer(), 
+                    peptide_rows    = integer())
+      
     for(i in 1:dim(proteins)[1])
     {
       proteins[i,-which('peptides' == names(proteins))] %>%
         writeData(wb = wb, sheet = dirs[j], startRow = nextRow, startCol = 1, rowNames = FALSE,
                   colNames = i == 1)
     
-      # adjustment and formatting for column names on the first protein
+      # account for header row on the first protein
       if(i == 1)
       {
         nextRow <- nextRow + 1
-        
-        addStyle(wb = wb, sheet = dirs[j], style = protein_header,
-                 rows = 1, cols = 1:(dim(proteins)[2] - 1),
-                 gridExpand = TRUE)
       }
-      
-      # format new protein row
-      addStyle(wb = wb, sheet = dirs[j], style = protein_rows,
-               rows = nextRow, cols = 1:(dim(proteins)[2] - 1),
-               gridExpand = TRUE)
+      indices$protein_rows <- c(indices$protein_rows, nextRow)
       
       # add peptides
       proteins$peptides[[i]] %>%
         select(-PROTEIN) %>%
         writeData(wb = wb, sheet = dirs[j], startRow = nextRow + 1, startCol = 2, rowNames = FALSE)
-      
-      # format peptides
-      addStyle(wb = wb, sheet = dirs[j], style = peptide_header,
-               rows = nextRow + 1,
-               cols = 2:dim(proteins$peptides[[i]])[2],
-               gridExpand = TRUE)
-      addStyle(wb = wb, sheet = dirs[j], style = peptide_rows,
-               rows = nextRow + 2:(dim(proteins$peptides[[i]])[1] + 1),
-               cols = 2:dim(proteins$peptides[[i]])[2],
-               gridExpand = TRUE)
+      indices$peptide_headers <- c(indices$peptide_headers, nextRow + 1)
       
       # group and hide peptides
       groupRows(wb, dirs[j], nextRow + 1:(dim(proteins$peptides[[i]])[1] + 1), hidden = TRUE)
@@ -244,11 +229,42 @@ foreach(j=1:length(paths)) %dopar%
       # update next row
       nextRow <- nextRow + dim(proteins$peptides[[i]])[1] + 2
     }
+    
+    indices$peptide_rows <- with(indices, 
+                                 c((1:max(peptide_headers)+1)[-c(protein_rows, peptide_headers) + 1],
+                                   max(peptide_headers) + 1:dim(proteins$peptides[[i]])[1]))
   
     # Write data to excel
     #openXL(wb)
-    saveWorkbook(wb, paste0('output/', dirs[j], '.xlsx'))
+    save(wb, indices, file = paste0('output/', dirs[j], '_wb.RData'))
   }
+  
+  ###############
+  # Format Rows #
+  ###############
+  
+  # format proteins
+  addStyle(wb = wb, sheet = dirs[j], style = protein_header_style,
+           rows = 1, cols = 1:dim(proteins)[2],
+           gridExpand = TRUE)
+  addStyle(wb = wb, sheet = dirs[j], style = protein_rows_style,
+           rows = indices$protein_rows,
+           cols = 1:dim(proteins)[2],
+           gridExpand = TRUE)
+  
+  # format peptides
+  addStyle(wb = wb, sheet = dirs[j], style = peptide_header_style,
+           rows = indices$peptide_headers,
+           cols = 2:max(which(!is.na(proteins[2,]))),
+           gridExpand = TRUE)
+  addStyle(wb = wb, sheet = dirs[j], style = peptide_rows_style,
+           rows = indices$peptide_rows,
+           cols = 2:max(which(!is.na(proteins[2,]))),
+           gridExpand = TRUE)
+  
+  
+  # save this monster
+  saveWorkbook(wb, file = paste0('output/', dirs[j], '.xlsx'))
   
   TRUE
 }
