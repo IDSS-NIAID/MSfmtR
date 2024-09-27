12b81c57af123a5854850c97db996c6ffd61f385
Randy Johnson <Randall.Johnson@nih.gov>
2024-09-27 15:18:40 -0400

    Add preprocess flag to `process_raw`
    allowing ability to skip MSstats data preprocessing when converting from Specronaut format

--- R/process_raw.R
+++ R/process_raw.R
@@ -4,8 +4,12 @@
 #' @param config list of configuration parameters
 #' @param stage character path to checkpoint file
 #' @param save_intermediate logical save intermediate data
+#' @param preprocess logical preprocess data using MSstats
 #'
 #' @details This function processes raw data into MSstats format. If save_intermediate is TRUE, the processed data are also saved to the checkpoint file. This also has the side effect of changing/updating `config` in the calling environment.
+#'
+#' There are currently two tested functions for `convert_function`: `MSstatsConvert::SpectronauttoMSstatsFormat` and `MSfmtR::convert_spectronaut`. The `MSfmtR` version uses the abundance score from Spectronaut directly and does not perform any preprocessing.
+#'
 #' @return data frame of processed raw data
 #' @export
 #' @importFrom Biostrings readAAStringSet
@@ -17,7 +21,7 @@
 #' @importFrom stringr fixed str_split
 #' @importFrom utils combn
 process_raw <- function(config, stage = file.path(config$output_dir, config$processed_checkpoint),
-                        save_intermediate = TRUE)
+                        save_intermediate = TRUE, preprocess = TRUE)
 {
   # for those pesky no visible binding warnings
   if(FALSE)
@@ -58,21 +62,98 @@ process_raw <- function(config, stage = file.path(config$output_dir, config$proc
 
 
   # process raw data
-  data <- raw |>
-
+  if(preprocess)
+  {
     # convert raw data into MSstats format
-    SpectronauttoMSstatsFormat(intensity = 'NormalizedPeakArea',
-                               use_log_file = FALSE)
+    data <- SpectronauttoMSstatsFormat(raw,
+                                       intensity = 'NormalizedPeakArea',
+                                       use_log_file = FALSE)
 
-  # remove out-of-spec peptides (MSstats assumes one row for every run for each feature, even if the intensity value is missing
-  #                              therefore, use mutate and convert to NA instead of filter...actually mutate doesn't work)
-  data$Intensity[data$Intensity < config$lloq |
-                 data$Intensity > config$uloq] <- NA
+    # remove out-of-spec peptides (MSstats assumes one row for every run for each feature, even if the intensity value is missing
+    #                              therefore, use mutate and convert to NA instead of filter...actually mutate doesn't work)
+    data$Intensity[data$Intensity < config$lloq |
+                   data$Intensity > config$uloq] <- NA
 
-  # peptide-level data (see documentation of `MSstats::dataProcess` at https://www.bioconductor.org/packages/devel/bioc/vignettes/MSstats/inst/doc/MSstats.html)
-  data <- dataProcess(data,
-                      normalization = FALSE,
-                      use_log_file = FALSE)
+    # peptide-level data (see documentation of `MSstats::dataProcess` at https://www.bioconductor.org/packages/devel/bioc/vignettes/MSstats/inst/doc/MSstats.html)
+    data <- dataProcess(data,
+                        normalization = FALSE,
+                        use_log_file = FALSE)
+  }else{
+    # peptide-level data (see documentation of `MSstats::dataProcess` at https://www.bioconductor.org/packages/devel/bioc/vignettes/MSstats/inst/doc/MSstats.html)
+    raw <- raw |>
+
+      # Figure out unique ID for each FrgIon (not unique)
+      group_by(PG.ProteinAccessions, EG.PrecursorId, F.FrgIon) |>
+      mutate(FrgIon.uid = paste(EG.PrecursorId, F.FrgIon, F.Charge, F.FrgLossType, sep = "_") |>
+               factor() |>
+               as.numeric()) |>
+      ungroup() |>
+
+      # format information for FeatureLevelData
+      mutate(PROTEIN = factor(PG.ProteinAccessions),
+             PEPTIDE = str_replace(EG.PrecursorId, "_", "") |>
+               str_replace("_.", "_") |>
+               factor(),
+             TRANSITION = paste(F.FrgIon, FrgIon.uid, sep = '_') |>
+               factor(),
+             FEATURE = paste(PEPTIDE, TRANSITION, sep = '_') |>
+               factor(),
+             LABEL = factor('L'),                                                               ##### don't know what this is
+             GROUP = factor(R.Condition),
+             SUBJECT = factor(R.Replicate),
+             FRACTION = as.integer(1),                                                          ##### don't know what this is
+             originalRUN = factor(R.FileName, levels = unique(R.FileName[order(R.Condition)])), # this is the ordering MSstats uses
+             RUN = as.numeric(originalRUN) |> as.factor(),
+             censored = FALSE,
+             INTENSITY = ifelse(rep(config$normMeasure == 'NormalizedPeakArea', nrow(raw)),
+                                F.NormalizedPeakArea,
+                                F.NormalizedPeakHeight),
+             ABUNDANCE = log10(INTENSITY),
+             newABUNDANCE = ABUNDANCE,
+             predicted = as.numeric(NA))
+
+    # create FeatureLevelData and filter
+    FeatureLevelData <- raw |>
+
+      dplyr::select(PROTEIN, PEPTIDE, TRANSITION, FEATURE, LABEL, GROUP, RUN, SUBJECT,
+                    FRACTION, originalRUN, censored, INTENSITY, ABUNDANCE, newABUNDANCE, PG.Quantity) |> # only using PG.Quantity for protein-level data. Drop it later.
+
+      dplyr::filter(INTENSITY > config$lloq, # remove out-of-spec peptides
+                    INTENSITY < config$uloq)
+
+
+    # protein-level data
+    ProteinLevelData = FeatureLevelData |>
+
+      group_by(RUN, PROTEIN) |>
+      mutate(NumMeasuredFeature = n(),
+             MissingPercentage = sum(INTENSITY < 1) / n(),
+             more50missing = MissingPercentage > 0.5,
+             NumImputedFeature = sum(is.na(INTENSITY))) |>
+      ungroup() |>
+
+      group_by(GROUP, PROTEIN) |>
+      mutate(TotalGroupMeasurements = n()) |>
+      ungroup() |>
+
+      dplyr::select(RUN, PROTEIN, originalRUN, GROUP, SUBJECT, PG.Quantity,                          # only keep protein-specific info
+                    TotalGroupMeasurements, NumMeasuredFeature, MissingPercentage, more50missing) |> # plus some summary stats
+      unique() |>
+
+      mutate(LogIntensities = log10(PG.Quantity),
+             NumImputedFeature = 0) |>
+
+      dplyr::select(RUN, PROTEIN, LogIntensities, originalRUN, GROUP, SUBJECT, TotalGroupMeasurements,
+                    NumMeasuredFeature, MissingPercentage, more50missing, NumImputedFeature) |>
+
+      dplyr::rename(Protein = PROTEIN)
+
+
+    # final data object
+    data <- list(FeatureLevelData = dplyr::select(FeatureLevelData, -PG.Quantity),
+                 ProteinLevelData = ProteinLevelData,
+                 SummaryMethod = 'linear')
+  }
 
   # save data
   if(FALSE)
